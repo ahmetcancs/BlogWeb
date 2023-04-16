b528ced6d37d0cf5b35b3002e11808a7be11382e
AHMET CAN <ahmetcan9816@gmail.com>
2023-04-16 11:08:32 +0300

    Login section created.

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -14,21 +15,30 @@ namespace BlogWeb
             DataSetTableAdapters.TBLABOUTTableAdapter dt = new DataSetTableAdapters.TBLABOUTTableAdapter(); 
             Repeater1.DataSource = dt.AboutList();
             Repeater1.DataBind();
+
             DataSetTableAdapters.TBLEXPERIENCETableAdapter dt2 = new DataSetTableAdapters.TBLEXPERIENCETableAdapter();
             Repeater2.DataSource = dt2.ListExperience();
             Repeater2.DataBind();
+
             DataSetTableAdapters.TBLEDUCATIONTableAdapter dt3 = new DataSetTableAdapters.TBLEDUCATIONTableAdapter();
             Repeater3.DataSource = dt3.ListEducation();
             Repeater3.DataBind();
+
             DataSetTableAdapters.TBLSKILLSTableAdapter dt4 = new DataSetTableAdapters.TBLSKILLSTableAdapter();
             Repeater4.DataSource = dt4.ListSkill();
             Repeater4.DataBind();
+
             DataSetTableAdapters.TBLHOBBIESTableAdapter dt5 = new DataSetTableAdapters.TBLHOBBIESTableAdapter();
             Repeater5.DataSource = dt5.ListHobby();
             Repeater5.DataBind();
+
             DataSetTableAdapters.TBLEVENTSTableAdapter dt6 = new DataSetTableAdapters.TBLEVENTSTableAdapter();
             Repeater6.DataSource = dt6.ListEvent();
             Repeater6.DataBind();
+
+            DataSetTableAdapters.TBLABOUTTableAdapter dt7 = new DataSetTableAdapters.TBLABOUTTableAdapter();
+            Repeater7.DataSource = dt7.AboutList();
+            Repeater7.DataBind();
         }
     }
 }
---
+++ Login.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+namespace BlogWeb
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        SqlConnection sqlConnection = new SqlConnection(@"Data Source=AHMET;Initial Catalog=BlogWebDB;Integrated Security=True");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BtnLogin_Click(object sender, EventArgs e)
+        {
+            sqlConnection.Open();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM TBLADMIN WHERE USERNAME=@P1 and PASSWORD=@P2",sqlConnection);
+            cmd.Parameters.AddWithValue("@P1", TxtUsername.Text);
+            cmd.Parameters.AddWithValue("@P2", TxtPassword.Text);
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.Read())
+            {
+                Response.Redirect("AdminExperiences.Aspx");
+            }
+            else
+            {
+                Response.Write("Wrong Username or Password");
+            }
+            sqlConnection.Close();
+        }   
+    }
+}
